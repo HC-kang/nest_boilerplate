46ca295c8aa5fe187001e7bc4d8a7bd3452d1b62
HC-kang <weston0713@gmail.com>
2023-01-23 22:40:40 +0900

    Feat. config 수정중

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,7 +7,15 @@ import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    AuthModule,
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,9 +1,10 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.RDS_HOSTNAME || 'localhost',
-  port: parseInt(process.env.REDS_PORT) || 5432,
+  port: parseInt(process.env.RDS_PORT) || 5432,
   username: process.env.RDS_USERNAME || 'heechan',
   password: process.env.RDS_PASSWORD || 'root',
   database: process.env.RDS_DB_NAME || 'boilerplate',
