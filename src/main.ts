b4e614981e1c0bbf89db36952359af66c6a9fc0c
HC-kang <weston0713@gmail.com>
2023-03-02 09:02:07 +0900

    Feat: main 작업중

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,57 @@ import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as winston from 'winston';
 import { utilities, WinstonModule } from 'nest-winston';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as expressBasicAuth from 'express-basic-auth';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+class Application {
+  private logger = new Logger(Application.name);
+  private DEV_MODE: boolean;
+  private PORT: string;
+  private corsOriginList: string[];
+  private ADMIN_USER: string;
+  private ADMIN_PASSWORD: string;
+
+  constructor(private server: NestExpressApplication) {
+    this.server = server;
+
+    if (!process.env.SECRET_KEY) this.logger.error('Set "SECRET" env');
+    this.DEV_MODE = process.env.NODE_ENV === 'production';
+    this.PORT = process.env.PORT || '5000';
+    this.corsOriginList = process.env.CORS_ORIGIN_LIST
+      ? process.env.CORS_ORIGIN_LIST.split(',').map((origin) => origin.trim())
+      : ['*'];
+    this.ADMIN_USER = process.env.ADMIN_USER || 'ford10082';
+    this.ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || '1234';
+  }
+  private setupBasicAuth() {
+    this.server.use(
+      ['/docs', '/docs-json'],
+      expressBasicAuth({
+        challenge: true,
+        users: {
+          [this.ADMIN_USER]: this.ADMIN_PASSWORD,
+        },
+      }),
+    );
+  }
+
+  private setupOpenAPIMiddelware() {
+    SwaggerModule.setup(
+      'docs',
+      this.server,
+      SwaggerModule.createDocument(
+        this.server,
+        new DocumentBuilder()
+          .setTitle('Ford - API')
+          .setDescription('Ford - API')
+          .setVersion('0.0.0')
+          .build(),
+      ),
+    );
+  }
+}
 
 async function bootstrap() {
   const logger = new Logger();
